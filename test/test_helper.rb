9788db4aed19ac174591272e98a600b5443c729a
Ryan Graham <r.m.graham@gmail.com>
2011-10-25 14:15:49 -0700

    Make dummy app tests run properly

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,13 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
+#Run any available migration
+ActiveRecord::Migrator.migrate 'up'
+
+# Set fixtures root
+ActiveSupport::TestCase.fixture_path=(File.expand_path("../dummy/test/fixtures",  __FILE__))
+ActiveSupport::TestCase.fixtures :all
+
 Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
