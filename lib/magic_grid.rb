69ce2c2b16a3c5ab2923292b0cea9566c2d1a75d
Ryan Graham <r.m.graham@gmail.com>
2011-10-25 08:57:16 -0700

    Cleanup a bit

--- lib/magic_grid.rb
+++ lib/magic_grid.rb
@@ -1,6 +1,5 @@
-require 'action_view/helpers/magic_grid_helper'
-
 module MagicGrid
 end
 
-ActionView::Helpers.send :include, ActionView::Helpers::MagicGridHelper
+require 'magic_grid/magic_grid_helpers'
+ActionView::Helpers.send :include, MagicGrid
--- test/magic_grid_test.rb
+++ test/magic_grid_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class MagicGridTest < ActiveSupport::TestCase
-  test "truth" do
+  test "Module definition" do
     assert_kind_of Module, MagicGrid
+  end
+  test "Class definition" do
+    assert_kind_of Class, MagicGrid::MagicGridHelpers::MagicGrid
+  end
+  test "ActionView extension" do
+    assert_kind_of Module, ActionView::Helpers::MagicGridHelpers
+    assert_kind_of Class, ActionView::Helpers::MagicGridHelpers::MagicGrid
   end
 end
