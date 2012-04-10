20f62f75eaa01d24043a292183dd3ec63c320374
Dennis Taylor <dennis@funkplanet.com>
2012-04-09 17:09:34 -0700

    Added JS 'loaded' event for newly populated grids.

--- test/dummy/app/assets/javascripts/posts.js
+++ test/dummy/app/assets/javascripts/posts.js
@@ -2,4 +2,7 @@
 // All this logic will automatically be available in application.js.
 $(function () {
     $("#user_id").addClass("ready");
+    $(".magic_grid").on("magic_grid:loaded", function () {
+        console.log("Loaded a grid: ", this);
+    });
 });
