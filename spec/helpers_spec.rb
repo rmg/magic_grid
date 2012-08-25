651eb6028ce562a27d0c79bebcdc36f5064a89e1
Ryan Graham <r.m.graham@gmail.com>
2012-08-24 23:41:26 -0700

    Convert indentation to spaces
    Apparently I don't have Sublime setup properly yet :-(

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -4,40 +4,41 @@ require 'action_controller'
 
 describe MagicGrid::Helpers do
 
-	# Let's use the helpers the way they're meant to be used!
-	include MagicGrid::Helpers
+  # Let's use the helpers the way they're meant to be used!
+  include MagicGrid::Helpers
 
-	let(:empty_collection) { [] }
-	let(:column_list) { [:name, :description] }
+  let(:empty_collection) { [] }
+  let(:column_list) { [:name, :description] }
 
-	describe "#normalize_magic" do
+  describe "#normalize_magic" do
 
-		it "should turn an array into a MagicGrid::Definition" do
-			expect(normalize_magic([])).to be_a(MagicGrid::Definition)
-		end
+    it "should turn an array into a MagicGrid::Definition" do
+      expect(normalize_magic([])).to be_a(MagicGrid::Definition)
+    end
 
-		it "should give back the MagicGrid::Definition given, if given one" do
-			definition = normalize_magic([])
-			expect(normalize_magic(definition)).to be(definition)
-		end
-	end
+    it "should give back the MagicGrid::Definition given, if given one" do
+      definition = normalize_magic([])
+      expect(normalize_magic(definition)).to be(definition)
+    end
+  end
 
-	describe "#magic_collection" do
-		pending "should probably be removed, it's not really used"
+  describe "#magic_collection" do
+    pending "should probably be removed, it's not really used"
 
-		it "should give back a collection like the one given" do
-			my_empty_collection = empty_collection
-			expect(magic_collection(my_empty_collection, column_list)).to eq(my_empty_collection)
-		end
+    it "should give back a collection like the one given" do
+      my_empty_collection = empty_collection
+      expect(magic_collection(my_empty_collection, column_list)).to eq(my_empty_collection)
+    end
 
-	end
+  end
 
-	describe "#magic_grid" do
-		pending "DOES WAY TOO MUCH!!"
+  describe "#magic_grid" do
+    pending "DOES WAY TOO MUCH!!"
 
-		it "should barf without any arguments" do
-			expect { magic_grid }.to raise_error
-		end
-	end
+    it "should barf without any arguments" do
+      expect { magic_grid }.to raise_error
+    end
+
+  end
 
 end
