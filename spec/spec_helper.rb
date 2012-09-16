099cf507cda4f5b976fd33af48f60ba7d9ef7d85
Ryan Graham <r.m.graham@gmail.com>
2012-09-16 11:26:29 -0700

    Stub out ActionView::OutputBuffer for testing with Rails 3.0.x

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -53,6 +53,15 @@ module ActionFaker
   end
 end
 
+unless ActionView.const_defined?(:OutputBuffer)
+  puts "Defining ActionView::OutputBuffer"
+  # For testing Rails 3.0.x
+  module ActionView
+    class OutputBuffer < String
+    end
+  end
+end
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
