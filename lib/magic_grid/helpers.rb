56362758da17f9c906f19e27aecb81b94aa6bc9a
Ryan Graham <r.m.graham@gmail.com>
2012-10-09 09:53:18 -0700

    Revert "Removed unused current page attribute."
    This reverts commit 41f256af450e95a63eb9cbf67565f11946473b60.

--- lib/magic_grid/helpers.rb
+++ lib/magic_grid/helpers.rb
@@ -27,6 +27,7 @@ module MagicGrid
       base_params = grid.base_params
       data = {
         :searcher => grid.options[:searcher],
+        :current => url_for,
         :live_search => grid.options[:live_search],
         :listeners => (grid.options[:listeners] unless grid.options[:listeners].empty?),
         :remote => grid.options[:remote],
