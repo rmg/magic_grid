1f52bd4aba749e2690870ffbe434157b14cad768
Ryan Graham <r.m.graham@gmail.com>
2012-10-13 14:01:41 -0700

    Remove dead code from dropped Rails 3.0.x support

--- lib/magic_grid/railtie.rb
+++
@@ -1,9 +0,0 @@
-module MagicGrid
-  class Railtie < Rails::Railtie
-    # Once in production, on every page view in development
-    config.to_prepare do
-      # no caching based on path like require does
-      load 'magic_grid/helpers.rb'
-    end
-  end
-end
