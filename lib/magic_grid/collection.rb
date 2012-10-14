a80fc70d4719f3dc2ea0aa9996f761ef08f7d9c5
Ryan Graham <r.m.graham@gmail.com>
2012-10-13 17:41:13 -0700

    Separate search types

--- lib/magic_grid/collection.rb
+++ lib/magic_grid/collection.rb
@@ -16,39 +16,47 @@ module MagicGrid
       @collection = obj
     end
 
+    def search_using_builtin(q)
+      @collection.__send__(@grid.options[:search_method], q)
+    end
+
+    def search_using_where(q)
+      result = @collection
+      search_cols = @grid.options[:searchable].map do |searchable|
+        case searchable
+        when Symbol
+          known = @grid.columns.find {|col| col[:col] == searchable}
+          if known and known.key?(:sql)
+            known[:sql]
+          else
+            "#{table_name}.#{@collection.connection.quote_column_name(searchable.to_s)}"
+          end
+        when Integer
+          @grid.columns[searchable][:sql]
+        when String
+          searchable
+        else
+          raise "Searchable must be identifiable"
+        end
+      end
+
+      unless search_cols.empty?
+        begin
+          clauses = search_cols.map {|c| c << " LIKE :search" }.join(" OR ")
+          result = @collection.where(clauses, {:search => "%#{param(:q)}%"})
+        rescue
+          Rails.logger.debug "Given collection doesn't respond to :where well"
+        end
+      end
+      result
+    end
+
     def perform_search(q)
-      orig_collection = self
       begin
-        @collection = @collection.__send__(@grid.options[:search_method], q)
+        @collection = search_using_builtin(q)
       rescue
         Rails.logger.debug "Given collection doesn't respond to #{@grid.options[:search_method]} well"
-        @collection = orig_collection
-        search_cols = @grid.options[:searchable].map do |searchable|
-          case searchable
-          when Symbol
-            known = @grid.columns.find {|col| col[:col] == searchable}
-            if known and known.key?(:sql)
-              known[:sql]
-            else
-              "#{table_name}.#{@collection.connection.quote_column_name(searchable.to_s)}"
-            end
-          when Integer
-            @grid.columns[searchable][:sql]
-          when String
-            searchable
-          else
-            raise "Searchable must be identifiable"
-          end
-        end
-        unless search_cols.empty?
-          begin
-            clauses = search_cols.map {|c| c << " LIKE :search" }.join(" OR ")
-            @collection = @collection.where(clauses, {:search => "%#{param(:q)}%"})
-          rescue
-            Rails.logger.debug "Given collection doesn't respond to :where well"
-            @collection = orig_collection
-          end
-        end
+        @collection = search_using_where(q)
       end
       self
     end
