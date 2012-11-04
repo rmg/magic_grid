2b30ea2f5603771cb7d21429dd12e3c4933dc6a4
Ryan Graham <r.m.graham@gmail.com>
2012-11-04 11:11:46 -0800

    Cleanup HtmlGrid initializer

--- lib/magic_grid/html_grid.rb
+++ lib/magic_grid/html_grid.rb
@@ -16,7 +16,7 @@ module MagicGrid
         searcher: @grid.searcher,
         current: controller.request.fullpath,
         live_search: @grid.options[:live_search],
-        listeners: (@grid.options[:listeners] unless @grid.options[:listeners].empty?),
+        listeners: @grid.options[:listeners],
         remote: @grid.options[:remote],
         default_ajax_handler: @grid.options[:default_ajax_handler],
         params: @grid.base_params,
@@ -24,19 +24,13 @@ module MagicGrid
       grid_data_without_nils = grid_data.select {|_,v| v }
       table_classes = ['magic_grid'] << @grid.options[:class]
       @view.content_tag('table',
-                  class: table_classes.join(' '),
-                  id: @grid.magic_id,
-                  data: grid_data_without_nils
-                  ) do
-        table = @view.content_tag('thead', data: {params: @grid.base_params}) do
-          magic_grid_head
-        end
-        table << @view.content_tag('tbody', class: "ui-widget-content") do
-          magic_rows &block
-        end
-        table << @view.content_tag('tfoot') do
-          magic_grid_foot
-        end
+                        class: table_classes.join(' '),
+                        id: @grid.magic_id,
+                        data: grid_data_without_nils
+                        ) do
+        @view.content_tag('thead', data: {params: @grid.base_params}) { magic_grid_head } +
+          @view.content_tag('tbody', class: "ui-widget-content") { magic_rows &block } +
+          @view.content_tag('tfoot') { magic_grid_foot }
       end
     end
 
