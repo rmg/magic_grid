ed29b4d9eed86482984eefde0f898070ed465700
Ryan Graham <r.m.graham@gmail.com>
2013-03-21 18:18:59 -0700

    Only access @view via an accessor

--- lib/magic_grid/html_grid.rb
+++ lib/magic_grid/html_grid.rb
@@ -3,6 +3,8 @@ require 'magic_grid/order'
 
 module MagicGrid
   class HtmlGrid
+    attr_reader :view
+    private :view
 
     def initialize(grid_definition, view, controller = nil)
       @grid = grid_definition
@@ -30,25 +32,25 @@ module MagicGrid
         :id => @grid.magic_id,
         :data => grid_data.select {|_,v| v }
       }
-      @view.content_tag('table', table_options) do
+      view.content_tag('table', table_options) do
         thead + tbody(&row_renderer) + tfoot
       end
     end
 
     def thead
-      @view.content_tag('thead', :data => {:params => @grid.base_params}) do
+      view.content_tag('thead', :data => {:params => @grid.base_params}) do
         magic_grid_head
       end
     end
 
     def tbody(&row_renderer)
-      @view.content_tag('tbody', :class => "ui-widget-content") do
+      view.content_tag('tbody', :class => "ui-widget-content") do
         magic_rows &row_renderer
       end
     end
 
     def tfoot
-      @view.content_tag('tfoot') do
+      view.content_tag('tfoot') do
         magic_grid_foot
       end
     end
@@ -56,7 +58,7 @@ module MagicGrid
     def spinner_generator
       unless @spinner_drawn
         @spinner_drawn = true
-        @view.tag('span',
+        view.tag('span',
                   :id => (@grid.magic_id.to_s + "_spinner"),
                   :class => "magic_grid_spinner")
       end
@@ -87,8 +89,8 @@ module MagicGrid
     end
 
     def filler_block(content = nil, &block)
-      @view.content_tag 'tr' do
-        @view.content_tag('td', content,
+      view.content_tag 'tr' do
+        view.content_tag('td', content,
                           :class => 'full-width ui-widget-header',
                           :colspan => @grid.columns.count,
                           &block)
@@ -96,14 +98,14 @@ module MagicGrid
     end
 
     def magic_column_headers
-      @view.content_tag 'tr' do
+      view.content_tag 'tr' do
         @grid.columns.reduce(''.html_safe) do |acc, col|
           classes = ['ui-state-default'] << col.html_classes
           acc <<
           if col.sortable?
             sortable_header(col)
           else
-            @view.content_tag 'th', col.label.html_safe, :class => classes.join(' ')
+            view.content_tag 'th', col.label.html_safe, :class => classes.join(' ')
           end
         end
       end
@@ -119,8 +121,8 @@ module MagicGrid
 
     def render_empty_collection(fallback)
       if fallback
-        @view.content_tag 'tr' do
-          @view.content_tag('td', :colspan => @grid.columns.count,
+        view.content_tag 'tr' do
+          view.content_tag('td', :colspan => @grid.columns.count,
                             :class => 'if-empty') do
             if fallback.respond_to? :call
               fallback.call(@grid).to_s
@@ -134,16 +136,16 @@ module MagicGrid
 
     def grid_row(record, &row_renderer)
       if row_renderer
-        @view.capture(record, &row_renderer)
+        view.capture(record, &row_renderer)
       else
-        @view.content_tag 'tr', :class => @view.cycle('odd', 'even') do
+        view.content_tag 'tr', :class => view.cycle('odd', 'even') do
           @grid.columns.map { |c| grid_cell(c, record) }.join.html_safe
         end
       end
     end
 
     def grid_cell(column, record)
-      @view.content_tag('td', :class => column.html_classes) do
+      view.content_tag('td', :class => column.html_classes) do
         method = column.reader
         if method.respond_to? :call
           method.call(record).to_s
@@ -156,7 +158,7 @@ module MagicGrid
     end
 
     def order_icon(order = Order::Unordered)
-      @view.content_tag 'span', '', :class => "ui-icon #{order.icon_class}"
+      view.content_tag 'span', '', :class => "ui-icon #{order.icon_class}"
     end
 
     def column_link_params(col)
@@ -186,16 +188,16 @@ module MagicGrid
       else
         order = Order::Unordered
       end
-      @view.content_tag 'th', :class => classes.join(' ') do
-        @view.link_to params, :remote => @grid.options[:remote] do
+      view.content_tag 'th', :class => classes.join(' ') do
+        view.link_to params, :remote => @grid.options[:remote] do
           label.html_safe << order_icon(order)
         end
       end
     end
 
     def searcher_block(&spinner)
-      @view.content_tag('tr') do
-        @view.content_tag('td', :class => 'searcher full-width ui-widget-header',
+      view.content_tag('tr') do
+        view.content_tag('td', :class => 'searcher full-width ui-widget-header',
                     :colspan => @grid.columns.count) do
           searcher_input(&spinner)
         end
@@ -207,16 +209,16 @@ module MagicGrid
         :min_length => @grid.options[:min_search_length],
         :current => @grid.current_search || "",
       }
-      searcher = @view.label_tag(@grid.searcher.to_sym,
+      searcher = view.label_tag(@grid.searcher.to_sym,
                            @grid.options[:searcher_label])
-      searcher << @view.search_field_tag(@grid.searcher.to_sym,
+      searcher << view.search_field_tag(@grid.searcher.to_sym,
         @grid.param(:q),
         :placeholder => @grid.options[:searcher_tooltip],
         :size => @grid.options[:searcher_size],
         :data => searcher_data,
         :form => "a form that doesn't exist")
       if @grid.options[:search_button]
-        searcher << @view.button_tag(@grid.options[:searcher_button],
+        searcher << view.button_tag(@grid.options[:searcher_button],
           :class => 'magic-grid-search-button')
       end
       searcher << yield if block_given?
@@ -224,12 +226,12 @@ module MagicGrid
     end
 
     def magic_pager(collection, opts={})
-      if @view.respond_to? :will_paginate
+      if view.respond_to? :will_paginate
         # WillPaginate
-        @view.will_paginate collection.collection, opts
-      elsif @view.respond_to? :paginate
+        view.will_paginate collection.collection, opts
+      elsif view.respond_to? :paginate
         #Kaminari, or something else..
-        @view.paginate collection.collection, opts
+        view.paginate collection.collection, opts
       else
         ("<!-- page #{collection.current_page} of #{collection.total_pages} -->" +
          '<!-- INSTALL WillPaginate or Kaminari for a pager! -->').html_safe
@@ -237,15 +239,15 @@ module MagicGrid
     end
 
     def magic_pager_block(&spinner)
-      @view.content_tag('tr') do
-        @view.content_tag('td', :class => 'full-width ui-widget-header magic-pager',
+      view.content_tag('tr') do
+        view.content_tag('td', :class => 'full-width ui-widget-header magic-pager',
                     :colspan => @grid.columns.count) do
           pager = magic_pager(@grid.magic_collection,
                                 :param_name => @grid.param_key(:page),
                                 :params => @grid.base_params
                                )
           if spinner
-            pager << @view.capture(&spinner)
+            pager << view.capture(&spinner)
           end
           pager
         end
